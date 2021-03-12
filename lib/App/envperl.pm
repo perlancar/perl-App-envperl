da94115c6ca9baadeea888f9ec610571a941e8cc
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-03-13 06:51:58 +0700

    Initial commit

---
+++ lib/App/envperl.pm
@@ -0,0 +1,13 @@
+package App::envperl;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Print environment variables as seen by a Perl script
+
+=head1 SYNOPSIS
+
+See the included script L<envperl>.
